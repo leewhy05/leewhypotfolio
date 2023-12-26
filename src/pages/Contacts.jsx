dde5a33ffc616043d97daad72429e27eda90e0dd
leewhy05 <leewhy1105@gmail.com>
2023-12-26 03:20:12 -0700

    fixes

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import eye from '../assets/myface.jpeg'
-import message from '../assets/message.png'
+import messag from '../assets/message.png'
 import send from '../assets/send.png'
 import github from '../assets/github.png'
+import axios from "axios"
+import toast from 'react-hot-toast'
 
 
 const Contacts = () => {
+  const [from, setFrom] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+  const navigate = useNavigate()
+ 
+  async function handleSubmit(e) {
+    e.preventDefault()
+      try {
+         const data = await axios.post('https://portfolio-e50u.onrender.com/api/message', {
+            from,
+            subject,
+            message
+          })
+         console.log(data);
+         if(data.status === 201){
+            toast.success(data.data.msg);
+            navigate('/')
+           
+         }
+
+      } catch (error) {
+        console.log(error);
+        toast.error(error.response.data.msg.message);
+      }
+        
+    }
   return (
     <div>
       <div className='container mt-4'>
@@ -36,7 +64,7 @@ From implementing user interfaces to optimizing backend systems, I take pride in
     <Link className='text-decoration-none d-none d-lg-block'><img src={eye} alt={eye} className='img-fluid img' /></Link>
     <div className='d-flex gap-3'>
     <Link className='d-flex align-items-center gap-1 text-decoration-none text-light'>
-        <img src={message} alt=""className='img-fluid lod' />
+        <img src={messag} alt=""className='img-fluid lod' />
         <p className='pt-3'>Email Me</p>
         <img src={send} alt="" className='img-fluid lod' />
     </Link>
@@ -54,17 +82,23 @@ From implementing user interfaces to optimizing backend systems, I take pride in
 <h2>SEND A MESSAGE</h2>
 <Form.Group className="mb-3 mt-5" controlId="exampleForm.ControlInput1">
  <Form.Label>FROM</Form.Label>
- <Form.Control type="email" placeholder="your name" />
+ <Form.Control type="email" placeholder="your name" 
+ value={from}
+ onChange={(e)=>setFrom(e.target.value)} />
 </Form.Group>
 <Form.Group className="mb-3 mt-4" controlId="exampleForm.ControlInput1">
  <Form.Label>SUBJECT</Form.Label>
- <Form.Control type="email" placeholder="your purpose" />
+ <Form.Control type="text" placeholder="your purpose"
+ value={subject}
+ onChange={(e)=>setSubject(e.target.value)}  />
 </Form.Group>
 <Form.Group className="mb-3 mt-4" controlId="exampleForm.ControlTextarea1">
  <Form.Label>WRITE A MESSAGE</Form.Label>
- <Form.Control as="textarea" placeholder='write a message and add your email or phone number please' rows={5} />
+ <Form.Control as="textarea" placeholder='write a message and add your email or phone number please' rows={5}
+ value={message}
+ onChange={(e)=>setMessage(e.target.value)}  />
 </Form.Group>
-<button className='btn btn-light'>Send a message</button>
+<button className='btn btn-light' onClick={handleSubmit}>Send a message</button>
 </Form>  
 </div>
  </div>
